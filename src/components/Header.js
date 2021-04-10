99905e5f2e4e5a950bb627330b717adf2e4f1d0d
Marco Seguri <seguri@users.noreply.github.com>
2021-04-10 23:36:40 +0200

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className='header'>
+      <h1>Task Tracker</h1>
+      <button className='btn'>Add</button>
+    </header>
+  )
+}
+
+export default Header
+  
