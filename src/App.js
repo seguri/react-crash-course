a1979baacbf833ef1029bbe37d41b17368d8a5bf
Marco Seguri <seguri@users.noreply.github.com>
2021-04-11 17:54:47 +0200

    Delete task from list when clicking the FaTimes icon

--- src/App.js
+++ src/App.js
@@ -25,13 +25,17 @@ function App() {
   ]);
 
   const deleteTask = (id) => {
-    console.log("delete", id);
+    setTasks(tasks.filter((task) => task.id !== id));
   };
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No Tasks to show"
+      )}
     </div>
   );
 }
